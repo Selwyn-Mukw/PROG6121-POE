117d7eb104b780002b32574c26f486ce6d690339
Selwyn Mukwevho <ST10465950@rcconnect.edu.za>
2026-04-08 05:06:37 +0200

    Add text to speech greeting when you launch bot

--- chatBot/Program.cs
+++ chatBot/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace chatBot
+﻿using System;
+using System.Media;
+namespace chatBot
+    
+
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Cyber security Hub my Name is Ori and I will guide you through this project");
+            SoundPlayer player = new SoundPlayer(@"C:\Users\mukwe\Downloads\CyberGreeting.wav");
+            player.Load();
+            player.PlaySync();
+            //Console.WriteLine("Welcome to the Cyber security Hub my Name is Ori and I will teach you how to be safe online ");
         }
     }
 }
