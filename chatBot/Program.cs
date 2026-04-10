d77063d8c724f39151dc7e9c1e491291a989f83e
Selwyn Mukwevho <ST10465950@rcconnect.edu.za>
2026-04-10 05:09:10 +0200

    Create Menu of questions which the user can choose what topic they are interested in

--- chatBot/Program.cs
+++ chatBot/Program.cs
@@ -21,8 +21,23 @@ namespace chatBot
 
             Console.WriteLine($"Hello {name} Welcome to the cyber security hub.");
 
-            
+            //Menu of questions the user can ask the chatbot
+            string[] options = new string[]
+            {
+                "1. What is cyber security?",
+                "2. What is phishing?",
+                "3. What is malware?",
+                "4. What is ransomware?",
+                "5. What is a firewall?",
+                "6. What is a VPN?",
+                "7. how do I protect myself from cyber attacks?",
+                "8. How do I create a strong password?",
+                "9. What is two-factor authentication?",
+                "10. How can I protect my personal data online?",
+                "11. What is a social engineering attack?",
+                
 
+                }
             }
         }
     // create a class which will play an audio file when the user starts the program
@@ -36,6 +51,14 @@ namespace chatBot
             player.Play();
             //player.PlaySync();
         }
+    }
+
+    class user
+    {
+       
+
+
+
     }
     
 }
